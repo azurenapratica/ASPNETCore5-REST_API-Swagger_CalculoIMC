c8cc01ce9a38f4babd3446b0d1ef5be823892ff9
Renato Groffe <renato.groffe@yahoo.com.br>
2021-05-22 23:19:10 -0300

    .NET 5 + ASP.NET Core + REST API + Swagger + Dockerfile + Juros Compostos
    .NET 5 + ASP.NET Core + REST API + Swagger + Dockerfile + Juros Compostos

---
+++ APICalculoIMC/Controllers/CalculoIMCController.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using APICalculoIMC.Models;
+
+namespace APICalculoIMC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculoIMCController : ControllerBase
+    {
+        private readonly ILogger<CalculoIMCController> _logger;
+
+        public CalculoIMCController(ILogger<CalculoIMCController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("imc")]
+        [ProducesResponseType(typeof(ClassificacaoPessoa), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(FalhaCalculo), (int)HttpStatusCode.BadRequest)]
+        public ActionResult<ClassificacaoPessoa> Get(double peso, double altura)
+        {
+            _logger.LogInformation(
+                "Recebida nova requisição|" +
+               $"Peso em Kg: {peso}|" +
+               $"Altura em Metros: {altura}");
+
+            if (peso <= 0)
+                return new BadRequestObjectResult(new FalhaCalculo() { Mensagem = "O Peso em Kg deve ser maior do que zero!" });
+
+            if (altura <= 0)
+                return new BadRequestObjectResult(new FalhaCalculo() { Mensagem = "A Altura em Metros deve ser maior do que zero!" });
+
+            var resultado = new ClassificacaoPessoa(peso, altura);
+            _logger.LogInformation("Resultado -> " +
+                $"IMC: {resultado.IMC}|" +
+                $"Situação: {resultado.Situacao}");            
+            return resultado;
+        }
+    }
+}
---
+++ APICalculoIMC/Models/ClassificacaoPessoa.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace APICalculoIMC.Models
+{
+    public class ClassificacaoPessoa
+    {
+        public double Peso { get; init; }
+        public double Altura { get; init; }
+        public double IMC { get; init; }
+        public string Situacao { get; init; }
+
+        public ClassificacaoPessoa(double peso, double altura)
+        {
+            Peso = peso;
+            Altura = altura;
+            IMC = Peso / (Altura * Altura); // Simulação de falha
+            //IMC = Math.Round(Peso / (Altura * Altura), 2);
+            Situacao = IMC switch
+            {
+                < 18.5 => "Magreza",
+                < 25.0 => "Normal",
+                < 29.9 => "Sobrepeso",
+                < 40.0 => "Obesidade",
+                _ => "Obesidade Grave"
+            };
+        }
+    }
+}
